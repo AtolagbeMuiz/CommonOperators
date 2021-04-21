d7621cb70272ad37c3a142fec1aa2e274a5b1b17
Atolagbe Muiz <atolagbemuiz@gmail.com>
2021-04-21 10:22:50 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Common_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // +, *, -, %
+            // int numerator = 10;
+            // int denominator = 3;
+
+            // int result = numerator/denominator;
+            // System.Console.WriteLine(result);
+
+
+            // int x = 20;
+            // int y = 10;
+
+            // //tenary oerator
+
+            // string result = x > y? "x is greater than y" : "x is less than y";
+            // System.Console.WriteLine(result);
+
+
+            // int i = 5;
+            // var res = i+=5;
+            // System.Console.WriteLine(res);
+
+
+            //Increment
+            // int i = 5;
+            // var res = i++;
+
+            // System.Console.WriteLine(i);
+
+            //Decrement
+            int i = 5;
+            var res = i--;
+
+            System.Console.WriteLine(i);
+        }
+    }
+}
